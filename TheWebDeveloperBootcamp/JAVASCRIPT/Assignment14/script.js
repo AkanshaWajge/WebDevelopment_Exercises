686f631fc6777eca64e26e731ad6495538b2a87a
akansha wajge <akanshad.wajge@gmail.com>
2018-06-20 16:08:42 +0530

    Number Guessing game using Js

---
+++ TheWebDeveloperBootcamp/JAVASCRIPT/Assignment14/script.js
@@ -0,0 +1,10 @@
+var number = prompt("Guess a number");
+if (number < 7){
+    alert("Number too low");
+}
+else if (number > 7){
+    alert("Number too high");
+}
+else if (number = 7){
+    alert("You gussed it!!");
+}
