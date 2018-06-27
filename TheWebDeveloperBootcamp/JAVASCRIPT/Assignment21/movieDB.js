6a9e9ec07e7783ca8abcd92849bf57d4d9add7c1
akansha wajge <akanshad.wajge@gmail.com>
2018-06-27 15:24:45 +0530

    Javascript objects

--- TheWebDeveloperBootcamp/JAVASCRIPT/Assignment21/movieDB.js
+++ TheWebDeveloperBootcamp/JAVASCRIPT/Assignment21/movieDB.js
@@ -1,18 +1,18 @@
 var movies = [
     {
-        title: "Judaai",
+        title: "Frozen",
         reading: 1,
         hasWatched: true,
         rating: 5
     },
     {
-        title: "Ishq",
+        title: "Inside Out",
         reading: 0,
         hasWatched: false,
         rating: 3.2
     },
     {
-        title: "Hiss",
+        title: "How I met your mother",
         reading: 1,
         hasWatched: true,
         rating: 0
@@ -21,6 +21,10 @@ var movies = [
 ];
 for(i=0; i <= movies.length-1; i++){
     if(movies[i].hasWatched == true){
-        document.write("You have watched" + movies[i].title + "with rating -" + movies[i].title +"\n" );
+        document.write("You have watched " + movies[i].title + " with rating - " + movies[i].rating +" stars\n" );
+    }
+    else{
+        document.write("You have not seen " + movies[i].title + " with rating - " + movies[i].rating +"stars yet \n" );
+
     }
 }
